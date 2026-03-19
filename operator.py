f718340abfd1e66bb8e17019355a08811645169a
Swetu07 <swetankkumar15f@gmail.com>
2026-03-19 18:41:32 +0530

    Day 2: operator

---
+++ operator.py
@@ -0,0 +1,21 @@
+#arithmetic operator
+a = 3
+b = 4
+c = (a + b)
+print (c)
+
+#assignment operator
+a = 3
+b = 4
+b += 4 
+print ( a , b)
+
+#comparison operator
+d = 54 > 56
+e = 4 < 5
+print(d , e)
+
+
+#logical operator
+f = True or False
+print (f)
