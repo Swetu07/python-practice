e772d1a15e52882c0f1537927623cced208e7186
Swetu07 <swetankkumar15f@gmail.com>
2026-03-24 21:09:29 +0530

    Day 7: While loops

---
+++ whileloop.py
@@ -0,0 +1,15 @@
+# WHILE LOOP SYNTAX
+i = 1
+
+while(i<51):
+    print(i)
+    i +=1 # or i = i + 1
+
+# WHILE LOOP IN LIST
+l = [1, "Swet", False, "Koffee", "Wolf", "Roman", "Hardik"]
+
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    i +=1
