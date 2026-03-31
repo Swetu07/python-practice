db8d97d852bc81b2459417f65f9563dc9783299f
Swetu07 <swetankkumar15f@gmail.com>
2026-03-31 21:16:19 +0530

    Day 14: Sets

---
+++ sets.py
@@ -0,0 +1,22 @@
+## Sets
+e = set() # Dont use s = {} as it will create an empty dictionary
+s = {1, 5, 32, 54,5, 5, 5} 
+
+print(s)
+
+## Sets methoods
+s = {1, 5, 32, 54,5, 5, 5, "swet"}
+
+print(s, type(s))
+
+s.add(566)
+print(s, type(s))
+s.remove(1)
+print(s, type(s))
+
+## union and intersection
+s1 = {1, 45, 6, 78}  
+s2 = {7, 8, 1, 78}
+
+print(s1.union(s2))
+print(s1.intersection(s2))
